e43d215da8f33f99f7e7aae2d2c909415c8bdc2d
Asad Khaksar <119926803+asadkhaksar1122@users.noreply.github.com>
2024-08-06 17:50:43 +0500

    some folder added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: "D:/My coding/Project/My Protfolio",
   plugins: [react()],
+  base:"/vite=deploy/"
 });
